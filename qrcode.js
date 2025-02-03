3e34c0b060664655b8029cede405768738157fe1
ruslan_parhomenko <parhomenkogm@gmail.com>
2025-02-03 14:12:32 +0200

    new

---
+++ qrcode.js
@@ -0,0 +1,7 @@
+
+        new QRCode(document.getElementById("qrcode"), {
+            text: "https://ruslanparhomenko.github.io/meni.io/",
+            width: 128,
+            height: 128
+        });
+    
