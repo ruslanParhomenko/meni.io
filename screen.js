ea453c3a923e9d8daf961ecb48cc529a1cfc06d5
ruslan_parhomenko <parhomenkogm@gmail.com>
2025-02-05 11:01:27 +0200

    fix

--- screen.js
+++ screen.js
@@ -1,23 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const requestFullScreen = () => {
-        const elem = document.documentElement; // Весь документ
-        if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-        } else if (elem.mozRequestFullScreen) { // Firefox
-            elem.mozRequestFullScreen();
-        } else if (elem.webkitRequestFullscreen) { // Chrome, Safari
-            elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) { // IE/Edge
-            elem.msRequestFullscreen();
-        }
+// document.addEventListener("DOMContentLoaded", () => {
+//     const requestFullScreen = () => {
+//         const elem = document.documentElement; // Весь документ
+//         if (elem.requestFullscreen) {
+//             elem.requestFullscreen();
+//         } else if (elem.mozRequestFullScreen) { // Firefox
+//             elem.mozRequestFullScreen();
+//         } else if (elem.webkitRequestFullscreen) { // Chrome, Safari
+//             elem.webkitRequestFullscreen();
+//         } else if (elem.msRequestFullscreen) { // IE/Edge
+//             elem.msRequestFullscreen();
+//         }
 
-        // После первого касания удаляем обработчик, чтобы не срабатывать повторно
-        document.removeEventListener("touchstart", requestFullScreen);
-    };
+//         // После первого касания удаляем обработчик, чтобы не срабатывать повторно
+//         document.removeEventListener("touchstart", requestFullScreen);
+//     };
 
-    // Включаем fullscreen при первом касании экрана
-    document.addEventListener("touchstart", requestFullScreen);
-});
+//     // Включаем fullscreen при первом касании экрана
+//     document.addEventListener("touchstart", requestFullScreen);
+// });
 
 document.addEventListener("touchstart", () => {
     setTimeout(() => {
